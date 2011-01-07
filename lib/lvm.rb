193f7f470ded627f2a85f413f4687b57c63fc2e5
LS <>
2011-01-07 10:40:41 +0100

    Add the include of the Wrapper PVCreate and PVRemove

--- lib/lvm.rb
+++ lib/lvm.rb
@@ -3,26 +3,33 @@ require 'lvm/userland'
 require 'lvm/logical_volumes'
 require 'lvm/volume_groups'
 require 'lvm/physical_volumes'
+require 'lvm/wrapper/pvcreate'
+require 'lvm/wrapper/pvremove'
+
 
 module LVM
   VERSION = 'default'
 
   class LVM
+
+    include Wrapper::PVCreate
+    include Wrapper::PVRemove
+
     attr_reader :command
     attr_reader :server
     attr_reader :logical_volumes
     attr_reader :volume_groups
     attr_reader :physical_volumes
 
-    VALID_OPTIONS = [
-      :command,
-      :server,
-      :version,
-      :debug
+    VALID_OPTIONS   = [
+        :command,
+        :server,
+        :version,
+        :debug
     ]
 
     DEFAULT_COMMAND = '/usr/bin/sudo /sbin/lvm'
-    DEFAULT_SERVER = 'localhost'
+    DEFAULT_SERVER  = 'localhost'
 
     def initialize(options={})
       # handy, thanks net-ssh!
@@ -30,7 +37,7 @@ module LVM
       if invalid_options.any?
         raise ArgumentError, "invalid option(s): #{invalid_options.join(', ')}"
       end
-      @server = options[:server] || DEFAULT_SERVER
+      @server  = options[:server] || DEFAULT_SERVER
       @command = options[:command] || DEFAULT_COMMAND
 
       if @server == DEFAULT_SERVER
@@ -43,10 +50,10 @@ module LVM
 
       # default to loading attributes for the current version
       options[:version] ||= VERSION
-      options[:debug] ||= false
+      options[:debug]   ||= false
 
-      @logical_volumes = LogicalVolumes.new(options)
-      @volume_groups = VolumeGroups.new(options)
+      @logical_volumes  = LogicalVolumes.new(options)
+      @volume_groups    = VolumeGroups.new(options)
       @physical_volumes = PhysicalVolumes.new(options)
 
       if block_given?
@@ -56,9 +63,12 @@ module LVM
       end
     end
 
+    # end initialize
+
+
     def raw(args)
       output = []
-      External.cmd(@server,"#{@command} #{args}") do |line|
+      External.cmd(@server, "#{@command} #{args}") do |line|
         output << line
       end
       if block_given?
@@ -77,12 +87,12 @@ module LVM
       userland = UserLand.new
       raw('version') do |line|
         case line
-        when %r{^\s+LVM version:\s+([0-9].*)$}
-          userland.lvm_version = $1
-        when %r{^\s+Library version:\s+([0-9].*)$}
-          userland.library_version = $1
-        when %r{^\s+Driver version:\s+([0-9].*)$}
-          userland.driver_version = $1
+          when %r{^\s+LVM version:\s+([0-9].*)$}
+            userland.lvm_version = $1
+          when %r{^\s+Library version:\s+([0-9].*)$}
+            userland.library_version = $1
+          when %r{^\s+Driver version:\s+([0-9].*)$}
+            userland.driver_version = $1
         end
       end
 
