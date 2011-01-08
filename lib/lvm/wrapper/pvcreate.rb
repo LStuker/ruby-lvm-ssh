3488bed1f0bfe2c6ff468144508c2a60230b0c40
LS <>
2011-01-08 12:59:28 +0100

    change methode naming for better code style

--- lib/lvm/wrapper/pvcreate.rb
+++ lib/lvm/wrapper/pvcreate.rb
@@ -1,4 +1,5 @@
 require 'lvm/wrapper'
+require 'lvm/wrapper/pvs'
 
 module LVM
   module Wrapper
@@ -7,10 +8,10 @@ module LVM
       # Create a LVM Physical Volume out of a device
       # if fdisk does not find a partition on the disk and
       # if pvscan does not find an existing physical volume label
-      def pvcreate(devicename)
+      def pv_create(devicename)
 
-        if checkDeviceOnPartition(devicename)
-          if checkDeviceOnPV(devicename)
+        if partition_check_device(devicename)
+          if pv_check_device(devicename)
             External.cmd(@server, "#{@command} pvcreate #{devicename}")
           end
         end
@@ -18,13 +19,13 @@ module LVM
       end
 
       # Check with pvscan if there is a lvm label on the device
-      def checkDeviceOnPV(devicename)
+      def pv_check_device(devicename)
         External.cmd(@server, "#{@command} pvscan #{devicename}").include? "No matching physical volumes found"
       end
       module_function :checkDeviceOnPV
 
       # Check with fdsik if there is a partition on the device
-      def checkDeviceOnPartition(devicename)
+      def partition_check_device(devicename)
         External.cmd(@server, "/usr/bin/sudo /sbin/fdisk -l #{devicename}").include? "Disk #{devicename} doesn't contain a valid partition table"
       end
       module_function :checkDeviceOnPartition
