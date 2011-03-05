03f92eebe5c7e87105c4195158a0ed5f710e1e70
LS <>
2011-03-05 09:24:21 +0100

    change to create physical volume with a disk object

--- lib/lvm/wrapper/pvcreate.rb
+++ lib/lvm/wrapper/pvcreate.rb
@@ -3,35 +3,29 @@ require 'lvm/wrapper/pvs'
 
 module LVM
   module Wrapper
-    module PVcreate
+    module PVCreate
 
       # Create a LVM Physical Volume out of a device
-      # if fdisk does not find a partition on the disk and
-      # if pvscan does not find an existing physical volume label
-      def pv_create(devicename)
-
-        if partition_check_device(devicename)
-          if pv_check_device(devicename)
-            External.cmd(@server, "#{@command} pvcreate #{devicename}")
-          end
-        end
-
+      # if disk has now partition and now physical volume label
+      def pv_create(device)
+        External.cmd(@server, "#{@command} pvcreate #{device.name}") if not device.partition && device.pv_label
       end
 
-      # Check with pvscan if there is a lvm label on the device
-      def pv_check_device(devicename)
-        External.cmd(@server, "#{@command} pvscan #{devicename}").include? "No matching physical volumes found"
-      end
+#
+#      # Check with pvscan if there is a lvm label on the device
+#      def pv_check_device(devicename)
+#        External.cmd(@server, "#{@command} pvscan #{devicename}").include? "No matching physical volumes found"
+#      end
+#
+#      module_function :pv_check_device
+#
+#      # Check with fdsik if there is a partition on the device
+#      def partition_check_device(devicename)
+#        External.cmd(@server, "/usr/bin/sudo /sbin/fdisk -l #{devicename}").include? "Disk #{devicename} doesn't contain a valid partition table"
+#      end
+#
+#      module_function :partition_check_device
 
-      module_function :pv_check_device
-
-      # Check with fdsik if there is a partition on the device
-      def partition_check_device(devicename)
-        External.cmd(@server, "/usr/bin/sudo /sbin/fdisk -l #{devicename}").include? "Disk #{devicename} doesn't contain a valid partition table"
-      end
-
-      module_function :partition_check_device
-
-    end # module PVcreate
+    end # module PVCreate
   end # module Wrapper
 end # module LVM
