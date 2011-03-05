771d81766305d9b7c63551eba759544ef05a4b86
LS <>
2011-03-05 09:27:16 +0100

    change to create volume group with a array of physical volume objects. It create only a volume group if all physical volumes are not used

--- lib/lvm/wrapper/vgcreate.rb
+++ lib/lvm/wrapper/vgcreate.rb
@@ -1,34 +1,40 @@
+require File.expand_path(File.dirname(__FILE__) + '/physical_volume_used.rb')
 require 'lvm/wrapper'
-require 'lvm/wrapper/pvs'
+require 'lvm/wrapper/vgs'
 
 module LVM
   module Wrapper
-    module PVCreate
+    module VGCreate
+    include PhysicalVolumeUsed
 
-      # Create a LVM Physical Volume out of a device
-      # if fdisk does not find a partition on the disk and
-      # if pvscan does not find an existing physical volume label
-      def vg_create(vgname, devices)
 
-        if partition_check_device(devices)
-          if pv_check_device(devices)
-            External.cmd(@server, "#{@command} vgcreate -s 32M #{vgname} #{devices}")
-          end
-        end
-
+      # Create a Volume Group with Extents Size of 32 MB.
+      # if physical volumes are note used.
+      # See vor vgcreate command http://linux.die.net/man/8/vgcreate
+      # -c eq. --clustered
+      # -s eq. --physicalextentsize
+      def vg_create(volume_group_name, physical_volumes)
+        External.cmd(@server, "#{@command} vgcreate -s 32M #{volume_group_name} #{physical_volumes.to_s}") if physical_volumes_unused(physical_volumes)
       end
 
-      def vg_create_cluster(vgname, devices)
-
-        if partition_check_device(devices)
-          if pv_check_device(devices)
-            External.cmd(@server, "#{@command} vgcreate -s 32M -c #{vgname} #{devices}")
-          end
-        end
-
+      # Create a Clustered Volume Group with Extents Size of 32 MB.
+      # if physical volumes are note used.
+      # See vor vgcreate command http://linux.die.net/man/8/vgcreate
+      # -c eq. --clustered
+      # -s eq. --physicalextentsize
+      def vg_create_cluster(volume_group_name, physical_volumes)
+        External.cmd(@server, "#{@command} vgcreate -s 32M -s #{volume_group_name} #{physical_volumes.to_s}") if physical_volumes_unused(physical_volumes)
       end
 
 
-    end # module PVCREATE
+      
+
+#      # Give a physicalvolumes_list back from an array with physicalvolumes
+#      # e.g. "/dev/sda /dev/sdb /dev/sdc"
+#      def physicalvolumes_list(physicalvolumes)
+#        physicalvolumes_list= physicalvolumes.join(" ")
+#      end
+
+    end # module VGCreate
   end # module Wrapper
 end # module LVM
