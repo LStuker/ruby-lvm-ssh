bfe30afaf16faf596d0cb3cf910b98053d549f1c
LS <>
2011-03-05 09:30:21 +0100

    change to remove volume group with a volume group object. It removes a volume group if volume group has no logical volume

--- lib/lvm/wrapper/vgremove.rb
+++ lib/lvm/wrapper/vgremove.rb
@@ -5,13 +5,12 @@ module LVM
   module Wrapper
     module VGRemove
 
-      # Create a LVM Physical Volume out of a device
-      # if fdisk does not find a partition on the disk and
-      # if pvscan does not find an existing physical volume label
-      def vg_remove(vgname)
-        External.cmd(@server, "#{@command} vgremove #{vgname}")
+      # Remove a empty LVM Volume Group.
+      # See vor vgremove command http://linux.die.net/man/8/vgremove
+      def vg_remove(volume_group)
+        #If there is no logical volume than the volumegroup can be removed
+        External.cmd(@server, "#{@command} vgremove #{volume_group.name}") if volume_group.logical_volumes.empty?
       end
-
 
     end # module VGRemove 
   end # module Wrapper
